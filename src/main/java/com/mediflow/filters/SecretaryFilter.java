2293ee5bae8fb14eff19ee1ce4aeb0344db1030f
AbdallahRdf <abdallahradfi@gmail.com>
2024-05-16 04:59:38 +0100

    added servlet filter for secretary

---
+++ src/main/java/com/mediflow/filters/SecretaryFilter.java
@@ -0,0 +1,42 @@
+package com.mediflow.filters;
+
+import com.mediflow.enums.HttpCustomVerbs;
+import com.mediflow.utils.Validator;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/secretary-servlet")
+public class SecretaryFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        if(
+                ((HttpServletRequest)servletRequest).getMethod().equals(HttpCustomVerbs.POST.toString()) &&
+                        (
+                                servletRequest.getParameter("id") == null ||
+                                        (servletRequest.getParameter("id") != null && servletRequest.getParameter("cin") != null)
+                        )
+        ){
+            if(Validator.isPersonInfoValid(
+                    servletRequest.getParameter("cin"),
+                    servletRequest.getParameter("firstName"),
+                    servletRequest.getParameter("lastName"),
+                    servletRequest.getParameter("email"),
+                    servletRequest.getParameter("phone")
+            )) {
+                filterChain.doFilter(servletRequest, servletResponse);
+            } else {
+                if(servletRequest.getParameter("id") == null){
+                    ((HttpServletResponse)servletResponse).sendRedirect("admin/secretary/addSecretary.jsp");
+                } else {
+                    ((HttpServletResponse)servletResponse).sendRedirect("admin/secretary/updateSecretary.jsp");
+                }
+            }
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+}
